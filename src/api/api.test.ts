97b1db842e7325dfe101ab0cda6628156ecd74eb
chinmaytrpth2 <chinmay.trpth2@gmail.com>
2020-05-23 16:01:00 +0530

    Added API Test file

---
+++ src/api/api.test.ts
@@ -0,0 +1,21 @@
+import imageUpload from './imageUpload';
+
+
+describe('Testing the Fetch "POST" function', () => {
+    let URL = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+
+
+    it('Should return a POST response', () => {
+        imageUpload(URL).then(res => {
+            expect(res).toBeTruthy();
+    })
+})
+
+    it('Should return a Image Address in response', () => {
+        imageUpload(URL).then(res => {
+            expect(res.url).toBeTruthy();
+    })
+})
+
+})
+
