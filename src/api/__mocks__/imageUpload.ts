6564d899101c884065b7255f48710d02446a9e87
chinmaytrpth2 <chinmay.trpth2@gmail.com>
2020-05-23 16:12:26 +0530

    Added __mocks__ to the API folder

---
+++ src/api/__mocks__/imageUpload.ts
@@ -0,0 +1,5 @@
+const imageUpload = async(url: string) => { 
+    if(url) return Promise.resolve({url: "A URL of Image"});
+}
+
+export default imageUpload;
--- src/api/api.test.ts
+++ src/api/api.test.ts
@@ -1,5 +1,7 @@
 import imageUpload from './imageUpload';
 
+jest.mock('./__mocks__/imageUpload');
+
 
 describe('Testing the Fetch "POST" function', () => {
     let URL = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
